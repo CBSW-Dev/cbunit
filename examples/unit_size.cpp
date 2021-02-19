a51fff7648403fdfc9a8c11af4ed555b7b5451ea
Chris BSW <Christopher.Burdett-Smith-Whitting@LucyZodion.com>
2021-02-19 21:37:14 +0000

    Added unit tests for size

---
+++ examples/unit_size.cpp
@@ -0,0 +1,16 @@
+#include "cbunit.hpp"
+#include "cbunit_macros.hpp"
+
+fixture("Size", []() {
+  scenario("should have the size language chain", []() {
+    uint32_t value;
+    expect(value).size.to.equal(4); //pass
+    expect(value).size.to.equal(9); //fail
+  });
+
+  scenario("should have the size language chain for different types", []() {
+    void* value;
+    expect(value).size.to.equal(sizeof(void*)); //pass
+    expect(value).size.to.equal(9); //fail
+  });
+});
