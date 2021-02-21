d2d21c769727ceb55cd0f21abe65cbc287ea89b9
Chris BSW <Christopher.Burdett-Smith-Whitting@LucyZodion.com>
2021-02-21 17:05:17 +0000

    Alter ExpectIs to use decayed types

--- include/Expect/Traits/ExpectIs.hpp
+++ include/Expect/Traits/ExpectIs.hpp
@@ -6,54 +6,24 @@
 #include <deque>
 namespace CBUnit
 {
-  template<class T> struct ExpectIsSequence
+  namespace
   {
-    static constexpr bool value = false;
-  };
-
-  template<class U, std::size_t N> struct ExpectIsSequence<std::array<U, N>>
-  {
-    static constexpr bool value = true;
-  };
-
-  template<class U> struct ExpectIsSequence<const std::vector<U>&>
-  {
-    static constexpr bool value = true;
-  };
-
-  template<class U> struct ExpectIsSequence<std::vector<U>>
-  {
-    static constexpr bool value = true;
-  };
-
-  template<class U> struct ExpectIsSequence<const std::list<U>&>
-  {
-    static constexpr bool value = true;
-  };
-
-  template<class U> struct ExpectIsSequence<std::list<U>>
-  {
-    static constexpr bool value = true;
-  };
-
-  template<class U> struct ExpectIsSequence<const std::deque<U>&>
-  {
-    static constexpr bool value = true;
-  };
-
-  template<class U> struct ExpectIsSequence<std::deque<U>>
-  {
-    static constexpr bool value = true;
-  };
+    template<class T> struct ExpectIsSequence: std::false_type {};
+    template<class U, std::size_t N> struct ExpectIsSequence<std::array<U, N>>: std::true_type {};
+    template<class U> struct ExpectIsSequence<std::vector<U>>: std::true_type {};
+    template<class U> struct ExpectIsSequence<std::list<U>>: std::true_type {};
+    template<class U> struct ExpectIsSequence<std::deque<U>>: std::true_type {};
+  }
 
   template <typename T> class ExpectIs
   {
   public:
-    static constexpr bool boolean = std::is_same<T, bool>::value;
-    static constexpr bool integer = std::is_integral<T>::value && !boolean;
-    static constexpr bool string = std::is_same<T, std::string>::value;
-    static constexpr bool pointer = std::is_pointer<T>::value;
-    static constexpr bool constCharArray = std::is_same<T, const char*>::value;
-    static constexpr bool sequence =  ExpectIsSequence<T>::value;
+    using DecayedType = typename std::remove_cv<typename std::remove_reference<T>::type>::type;
+    static constexpr bool boolean = std::is_same<DecayedType, bool>::value;
+    static constexpr bool integer = std::is_integral<DecayedType>::value && !boolean;
+    static constexpr bool string = std::is_same<DecayedType, std::string>::value;
+    static constexpr bool pointer = std::is_pointer<DecayedType>::value;
+    static constexpr bool constCharArray = std::is_same<DecayedType, const char*>::value;
+    static constexpr bool sequence =  ExpectIsSequence<DecayedType>::value;
   };
 }
