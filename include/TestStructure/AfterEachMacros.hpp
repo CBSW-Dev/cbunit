4b9f06ae9a63dee3bbad2522a11a83e9ae0ab579
Chris BSW <Christopher.Burdett-Smith-Whitting@LucyZodion.com>
2021-03-17 22:11:55 +0000

    Newlines at end of macro files

--- include/TestStructure/AfterEachMacros.hpp
+++ include/TestStructure/AfterEachMacros.hpp
@@ -5,3 +5,4 @@
   CBUNIT_FILEINFO                                                           \
   CBUNIT_LINEINFO                                                           \
   ::CBUnit::AfterEachDeclaration CBUNIT_CONCAT(_cbunit_after_each_, __COUNTER__)(__VA_ARGS__);
+  
--- include/TestStructure/BeforeEachMacros.hpp
+++ include/TestStructure/BeforeEachMacros.hpp
@@ -5,3 +5,4 @@
   CBUNIT_FILEINFO                                                           \
   CBUNIT_LINEINFO                                                           \
   ::CBUnit::BeforeEachDeclaration CBUNIT_CONCAT(_cbunit_before_each_, __COUNTER__)(__VA_ARGS__);
+  
