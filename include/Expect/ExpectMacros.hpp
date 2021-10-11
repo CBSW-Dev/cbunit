85fe0caf5fcb10bcb1953aa713125355eb1724e7
Christopher Burdett-Smith-Whitting <Christopher.Burdett-Smith-Whitting@arm.com>
2021-10-11 09:11:21 +0100

    Expect To Override macro
    Change-Id: Ic00ad7b7a9705e9d54c3b3d39c0764a4078f83c8

--- include/Expect/ExpectMacros.hpp
+++ include/Expect/ExpectMacros.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
 #define CBUNIT_EXPECT ::CBUnit::build_make_expect(__FILE__, __LINE__).expect
+#define CBUNIT_EXPECT_TO_OVERRIDE(Class, Logic) ExpectTo<Class, Logic>: public virtual ExpectBase<Class>
